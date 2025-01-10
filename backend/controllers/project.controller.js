21c5b691e18d98d52ce97ad25a060487c45f5b18
Abhik004 <abhiksamanta004@gmail.com>
2025-01-10 15:43:00 +0530

    socket.io_setup

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -7,7 +7,7 @@ import { validationResult } from 'express-validator';
 export const createProject = async (req, res) => {
 
     const errors = validationResult(req);
-
+ 
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
